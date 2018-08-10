ba34819bdb25e531421bd26536913e6442b992b4
Will Badart <wbadart@live.com>
2018-08-09 21:59:10 -0700

    Use sandbox version of hspec-discover

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,3 @@
 -- Automatic test discover as described by:
 -- https://hspec.github.io/hspec-discover.html
-{-# OPTIONS_GHC -F -pgmF /usr/bin/hspec-discover #-}
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
