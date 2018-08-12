aa4ccccdee1115ae131c0dcbd662450f72fa4c71
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-12 02:45:51 +0000

    Deploy wbadart/hdsk to github.com/wbadart/hdsk.git:gh-pages

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Revision history for hdsk
-
-## 0.0.1.0  -- 2018-07-25
-
-* First version. Released on an unsuspecting world.
--- test/Spec.hs
+++
@@ -1,3 +0,0 @@
--- Automatic test discover as described by:
--- https://hspec.github.io/hspec-discover.html
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
