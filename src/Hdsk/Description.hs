5e5b6d5baab93689f2239d7cf65255b497059192
Will Badart <wbadart@live.com>
2018-08-11 17:17:03 -0700

    Changed percentile to use fractional interpolation Switched from midpoint to fractional interpolation. I'm comfortable with the switch in part because this is the default for pandas.Series.quantile. My approach was informed by this lesson: https://web.stanford.edu/class/archive/anthsci/anthsci192/anthsci192.1064/handouts/calculating%20percentiles.pdf
    The new equation introduced a couple edge cases which were previously
    handled by the index arithmetic. While these cases must unfortunately be
    explicitly enumerated to a slight detriment to readability, their right
    hand sides have order(s) lower runtime complexity.

--- src/Hdsk/Description.hs
+++ src/Hdsk/Description.hs
@@ -14,6 +14,7 @@ module Hdsk.Description
 , std
 , percentile, q1, q3
 , median, iqr
+, noOutliers
 ) where
 
 import Data.List (sort)
@@ -39,15 +40,24 @@ std = sqrt . var
 percentile :: Double -> Vector Double -> Double
 -- ^ /O(n log n)/ Finds the @p@th percentile of the vector of
 -- doubles, i.e. the element which is greater than @p@% of the other
--- elements. If @p@% names an existing element, it is chosen, else the
--- mean of the two closest is given (i.e. using midpoint interpolation).
+-- elements. Uses fractional interpolation when @p@ does not generate a
+-- whole index.
+--
+-- Unfortunately, the equation for fractional interpolation breaks down both
+-- at the bounds of lists and for lists shorter than 2 elements. On the flip
+-- side, these special cases can be handled in /O(n)/ or even /O(1)/ time
+-- for the singleton case.
+percentile 0   xs = V.minimum xs
+percentile 100 xs = V.maximum xs
 percentile p xs | V.null xs = undefined
                 | vecLen xs == 1 = V.head xs
-                | whole idx = vecSorted xs V.! left
-                | otherwise = mean $ V.slice left 2 (vecSorted xs)
-  where idx = (p / 100) * vecLen xs
-        left = floor idx - if ceiling idx == vecLen xs then 1 else 0
-        whole x = x == fromIntegral (floor x) || p `elem` [25, 50, 75]
+                | whole idx = xx V.! k
+                -- Apply fractional interpolation
+                | otherwise = (1 - f) * (xx V.! k) + f * (xx V.! (k + 1))
+  where whole x = x == fromIntegral (floor x)
+        idx = p * vecLen xs / 100 - 0.5
+        k = floor idx; f = idx - fromIntegral k
+        xx = vecSorted xs
 
 median :: Vector Double -> Double
 -- ^ /O(n log n)/ Finds the median element the vector. Undefined for empty
@@ -65,6 +75,13 @@ q3 = percentile 75
 iqr :: Vector Double -> Double
 -- ^ /O(n log n)/ Finds the interquartile range of a vector of doubles.
 iqr = (-) <$> q3 <*> q1
+
+noOutliers :: Vector Double -> Vector Double
+-- /O(n log n)/ Gives a copy of the input vector with all of the outliers
+-- filtered out. Outliers are considered to be points beyond 1.5 IQR from
+-- the median
+noOutliers xs = V.filter (\x -> x >= med - 1.5*iq && x <= med + 1.5*iq) xs
+  where med = median xs; iq = iqr xs
 
 
 -- ===== Utilities ===== --
--- test/Hdsk/DescriptionSpec.hs
+++ test/Hdsk/DescriptionSpec.hs
@@ -11,8 +11,8 @@ import qualified Data.Vector as V
 import Test.QuickCheck (property, forAll, shuffle, choose, vector)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
-import Hdsk.Description (mean, var, std, percentile, q1, q3)
 import Hdsk.Util (shouldLieBetween, shouldBeUndefined)
+import Hdsk.Description
 
 spec :: Spec
 spec = do
@@ -61,13 +61,13 @@ spec = do
 
     it "finds the p-percentile of the list by sorting" $ do
       lpercentile 0   uut `shouldBe` 1
-      lpercentile 25  uut `shouldBe` 2
+      lpercentile 30  uut `shouldBe` 2
       lpercentile 50  uut `shouldBe` 3
-      lpercentile 75  uut `shouldBe` 4
+      lpercentile 70  uut `shouldBe` 4
       lpercentile 100 uut `shouldBe` 5
 
     it "works on all permutations of a list" $ property $
-      forAll (shuffle uut) (\xs -> lpercentile 10 xs == 1.5)
+      forAll (shuffle uut) (\xs -> lpercentile 30 xs == 2)
 
     it "gives the maximum for 100th percentile" $ property $
       \xs -> null xs || lpercentile 100 xs == maximum xs
@@ -76,7 +76,7 @@ spec = do
       \xs -> null xs || lpercentile 0 xs == minimum xs
 
     it "handles singleton vectors" $ property $
-      forAll (choose (0.0, 1.0)) (\p -> lpercentile p [0] == 0)
+      forAll (choose (0, 100)) (\p -> lpercentile p [0] == 0)
 
     -- it "for any p and for any singleton, the percentile is the value"
     -- ^ not sure how to express this just yet. Look at Gen combinators
@@ -85,15 +85,38 @@ spec = do
       shouldBeUndefined $ evaluate (percentile 50 V.empty)
 
 
+  let lmedian = median . V.fromList
+  describe "median" $ do
+
+    it "picks the middle element of an odd-lengthed list" $ do
+      lmedian [1, 2, 3] `shouldBe` 2
+      lmedian [3, 1, 2] `shouldBe` 2
+      lmedian [31, 111.1, 12, 13, -10] `shouldBe` 13
+
+    it "accurately interpolates between values in an even-length list" $ do
+      lmedian [1, 2, 3, 4] `shouldBe` 2.5
+      lmedian [3, 4, 2, 1] `shouldBe` 2.5
+      lmedian [141, 16, 1.7, 12] `shouldBe` 14
+
+    it "works on all permutations of the list" $ property $
+      forAll (shuffle uut) (\xs -> lmedian xs == 3)
+
+    it "gives the single element of a singleton vector" $ property $
+      forAll (vector 1) $ \xs -> lmedian xs == head xs
+
+    it "is undefined on empty lists" $
+      shouldBeUndefined $ evaluate (median V.empty)
+
+
   let lq1 = q1 . V.fromList
   let lq3 = q3 . V.fromList
   describe "q1" $ do
 
     it "finds the first quartile of a vector of doubles" $
-      lq1 uut `shouldBe` 2
+      lq1 uut `shouldBe` 1.75
 
     it "works on all permutations of a list" $ property $
-      forAll (shuffle uut) (\xs -> lq1 xs == 2)
+      forAll (shuffle uut) (\xs -> lq1 xs == 1.75)
 
     it "is always no greater than q3" $ property $
       \xs -> null xs || lq1 xs <= lq3 xs
@@ -108,10 +131,10 @@ spec = do
   describe "q3" $ do
 
     it "finds the third quartile of a vector of doubles" $
-      lq3 uut `shouldBe` 4
+      lq3 uut `shouldBe` 4.25
 
     it "works on all permutations of a list" $ property $
-        forAll (shuffle uut) $ \xs -> lq3 xs == 4
+        forAll (shuffle uut) $ \xs -> lq3 xs == 4.25
 
     it "is always at least q1" $ property $
       \xs -> null xs || lq3 xs >= lq1 xs
