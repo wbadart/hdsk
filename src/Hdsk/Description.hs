bd2175cb33f9ecb7d601b32b066694b5d5b6ee58
Will Badart <wbadart@live.com>
2018-08-13 01:15:34 -0700

    Cleaned a bunch of cruft from Description.hs Also made a correction to the select algorithm; len > k should have been (and now is) len >= k

--- src/Hdsk/Description.hs
+++ src/Hdsk/Description.hs
@@ -13,87 +13,73 @@ module Hdsk.Description
 , var
 , std
 , percentile
-, percentile'
--- , percentile, q1, q3
--- , median, iqr
--- , noOutliers
+, median
+, q1, q3, iqr
 ) where
 
-import Data.List (sort)
-import qualified Data.List as L
-import Data.Sequence (Seq( (:<|) ))
-import qualified Data.Sequence as S
+import Data.Sequence (Seq((:<|), Empty))
 import Data.Vector (Vector)
+
+import qualified Data.List as L
+import qualified Data.Sequence as S
 import qualified Data.Vector as V
 
 mean :: (Foldable f, Fractional n) => f n -> n
--- ^ /O(n)/ Computes the arithmetic mean of a vector of doubles.
+-- ^ /O(n)/ Computes the arithmetic mean of a collection of numbers.
 mean xs = sum xs / fromIntegral (length xs)
 
 var :: (Foldable f, Functor f, Floating n) => f n -> n
--- ^ /O(n)/ Computes the unbiased variance of a vector of doubles.
+-- ^ /O(n)/ Computes the unbiased variance of a collection of numbers.
 var xs = sum (fmap sqDiff xs) / fromIntegral (length xs - 1)
   where sqDiff x = (x - avg) ** 2; avg = mean xs
 
 std :: (Foldable f, Functor f, Floating n) => f n -> n
--- ^ /O(n)/ Computes the standard deviation of a vector of doubles.
+-- ^ /O(n)/ Computes the standard deviation of a collection of numbers.
 std = sqrt . var
 
-percentile :: (Eq n, RealFrac n) => n -> Vector n -> n
--- ^ /O(n log n)/ Finds the @p@th percentile of the vector of
--- doubles, i.e. the element which is greater than @p@% of the other
--- elements. Uses fractional interpolation when @p@ does not generate a
--- whole index.
---
--- Unfortunately, the equation for fractional interpolation breaks down
--- both at the bounds of lists and for lists shorter than 2 elements. On
--- the flip side, these special cases can be handled in /O(n)/ or even
--- /O(1)/ time for the singleton case.
-percentile 0   xs = V.minimum xs
-percentile 100 xs = V.maximum xs
-percentile p xs | V.null xs = undefined
-                | vecLen xs == 1 = V.head xs
-                | whole idx = xx V.! k
-                -- Apply fractional interpolation
-                | otherwise = (1 - f) * (xx V.! k) + f * (xx V.! (k + 1))
-  where whole x = x == fromIntegral (floor x)
-        idx = p * vecLen xs / 100 - 0.5
-        k = floor idx; f = idx - fromIntegral k
-        xx = vecSorted xs
-
-percentile' :: (Foldable p, Selectable p, RealFrac n) => n -> p n -> n
-percentile' 0   xs = minimum xs
-percentile' 100 xs = maximum xs
-percentile' p xs | whole idx = select k xs
-                 | otherwise = mean (select k xs, select (k + 1) xs)
+percentile :: (Foldable p, Selectable p, RealFrac n) => n -> p n -> n
+-- ^ /O(n)/ Selects the element which is greater than @p@% of the rest.
+-- When the @p@-th percentile does not land directly on a whole index,
+-- midpoint interpolation is used to average left and right side of the
+-- split.
+percentile 0   xs = minimum xs
+percentile 100 xs = maximum xs
+percentile p xs | whole idx = select k xs
+                | otherwise = mean (select k xs, select (k + 1) xs)
   where idx = p * fromIntegral (length xs) / 100 + 0.5
         k   = floor idx
 
-select :: (Foldable p, Selectable p, Ord a) => Int -> p a -> a
--- ^ /O(n)/ Simple implementation of quickselct on @Sequence@s.
--- Undefined on empty sequences.
-select k all | len + 1 == k = x
-             | len      > k = select k left
-             | otherwise    = select (k - len - 1) right
-  where (left, right) = partition (< x) xs
-        len = length left
-        x = Hdsk.Description.head all; xs = Hdsk.Description.tail all
-
-median :: Vector Double -> Double
--- ^ /O(n log n)/ Finds the median element the vector. Undefined for empty
--- vectors.
+median :: (Foldable p, Selectable p, RealFrac n) => p n -> n
+-- ^ /O(n)/ Finds the median element the collection.
 median = percentile 50
+
+q1 :: (Foldable p, Selectable p, RealFrac n) => p n -> n
+-- ^ /O(n)/ Finds the first quartile of a collection of numbers.
+q1 = percentile 25
+
+q3 :: (Foldable p, Selectable p, RealFrac n) => p n -> n
+-- ^ /O(n)/ Finds the third quartile of a collection of numbers.
+q3 = percentile 75
+
+iqr :: (Foldable p, Selectable p, RealFrac n) => p n -> n
+-- ^ /O(n)/ Inter-quartile range. The distance between the first and third
+-- quartiles.
+iqr = (-) <$> q3 <*> q1
 
 
 -- ===== Utilities ===== --
-vecLen :: Num n => Vector a -> n
-vecLen = fromIntegral . V.length
-
-vecSorted :: Ord a => Vector a -> Vector a
-vecSorted = V.fromList . sort . V.toList
-
 whole :: RealFrac a => a -> Bool
-whole x = x == fromIntegral (floor x)
+whole x = x == fromIntegral (floor x :: Int)
+
+select :: (Foldable p, Selectable p, Ord a) => Int -> p a -> a
+-- ^ /O(n)/ Simple implementation of quickselct (aka Hoare's algorithm or
+-- k-rank). Selects the @k@-smallest element from the collection.
+select k xxs | len + 1 == k = x
+             | len     >= k = select k left
+             | otherwise    = select (k - len - 1) right
+  where (left, right) = partition (< x) xs
+        len = length left
+        x = Hdsk.Description.head xxs; xs = Hdsk.Description.tail xxs
 
 
 class Foldable p => Selectable p where
@@ -106,9 +92,11 @@ instance Selectable Vector where
   head      = V.head
   tail      = V.tail
 instance Selectable Seq where
-  partition = S.partition
+  partition     = S.partition
   head (x:<|_)  = x
+  head Empty    = error "empty list"
   tail (_:<|xs) = xs
+  tail Empty    = error "empty list"
 instance Selectable [] where
   partition = L.partition
   head      = L.head
