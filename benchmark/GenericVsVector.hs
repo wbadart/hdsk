1cc7af08427ac1f192179dec62cfe0975a36ad5f
Will Badart <wbadart@live.com>
2018-08-12 22:30:33 -0700

    Experiment with description implementations I found out that by using the base package implementations of the foldable functions (sum, length, etc.) I could shave a millisecond off the runtime for the descriptive stats. The function is still passed a vector, but there must be some very light, O(1), overhead that goes into the vector library implementations.
    The fact that this gives a (minute) performance bump, and allows the
    functions to be applied on a broader scale is a big win, and I'll be
    switching the implementation to this strategy over the next few commits.

--- benchmark/GenericVsVector.hs
+++ benchmark/GenericVsVector.hs
@@ -23,18 +23,33 @@ mkEnv = mkEnvN 1000000
 main = defaultMain
   [ env mkEnv $ \ ~(xs, vec) -> bgroup "mean"
     [ bench "generic" $ nf (genericMean::[Double]->Double) xs
-    , bench "vector"  $ nf mean vec ]
+    , bench "vector"  $ nf mean vec
+    , bench "foldable" $ nf mean3 vec ]
 
   , env mkEnv $ \ ~(xs, vec) -> bgroup "variance"
     [ bench "generic" $ nf (genericVar::[Double]->Double) xs
-    , bench "vector"  $ nf var vec ]
+    , bench "vector"  $ nf var vec
+    , bench "foldable" $ nf var3 vec ]
 
   , env mkEnv $ \ ~(xs, vec) -> bgroup "standard deviation"
     [ bench "generic" $ nf (genericStd::[Double]->Double) xs
-    , bench "vector"  $ nf std vec ]]
+    , bench "vector"  $ nf std vec
+    , bench "foldable" $ nf std3 vec ]]
 
 
 -- Utilities
 
 sample :: Int -> IO [Double]
 sample n = replicateM n $ randomRIO (1, 6)
+
+mean3 :: (Foldable f, Fractional n) => f n -> n
+mean3 xs = sum xs / fromIntegral (length xs)
+
+var3 :: (Functor f, Foldable f, Floating n) => f n -> n
+-- ^ /O(n)/ Computes the unbiased variance of a vector of doubles.
+var3 xs = let avg = mean3 xs; sqDiff x = (x - avg) ** 2
+          in sum (fmap sqDiff xs) / fromIntegral (length xs - 1)
+
+std3 :: (Functor f, Foldable f, Floating n) => f n -> n
+-- ^ /O(n)/ Computes the standard deviation of a vector of doubles.
+std3 = sqrt . var3
