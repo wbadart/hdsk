2165911bb5cdfb6c1568e01bb80330d4f1ee3ba9
Will Badart <wbadart@live.com>
2018-08-13 01:28:01 -0700

    Fixed ambiguous type errors Now need to fix the actual expectations

--- test/Hdsk/DescriptionSpec.hs
+++ test/Hdsk/DescriptionSpec.hs
@@ -19,37 +19,34 @@ spec :: Spec
 spec = do
 
   -- ===== MEAN ===== --
-  let lmean = mean . V.fromList
   describe "mean" $ do
 
     it "averages the doubles in a vector" $ do
-      lmean [1, 2, 3] `shouldBe` 2.0
-      lmean [10, 20, 15, 31] `shouldBe` 19.0
+      mean [1, 2, 3] `shouldBe` 2.0
+      mean [10, 20, 15, 31] `shouldBe` 19.0
 
     it "is undefined on empty vectors" $
       shouldBeUndefined $ evaluate (mean V.empty)
 
 
   -- ===== VARIANCE ===== --
-  let lvar = var . V.fromList
   describe "var" $ do
 
     it "correctly calculates the variance of a vector of doubles" $ do
-      lvar [1, 2, 3] `shouldBe` 1
-      lvar [10, 10, 10] `shouldBe` 0
+      var [1, 2, 3] `shouldBe` 1
+      var [10, 10, 10] `shouldBe` 0
 
     it "is undefined on empty vectors" $
       shouldBeUndefined $ evaluate (var V.empty)
 
 
   -- ===== STANDARD DEVIATION ===== --
-  let lstd = std . V.fromList
   describe "std" $ do
 
     it "correctly calculates the standard deviation of the vector" $ do
-      lstd [1, 2, 3] `shouldBe` 1
-      shouldLieBetween 1.58 1.59 (lstd [1, 2, 3, 4, 5])
-      lstd [10, 10, 10] `shouldBe` 0
+      std [1, 2, 3] `shouldBe` 1
+      shouldLieBetween 1.58 1.59 (std [1, 2, 3, 4, 5])
+      std [10, 10, 10] `shouldBe` 0
 
     it "is undefined on empty vectors" $
       shouldBeUndefined $ evaluate (std V.empty)
@@ -57,27 +54,27 @@ spec = do
 
   -- ===== PERCENTILE FAMILY ===== --
   let uut = [1, 2, 3, 4, 5]
-  let lpercentile p xs = percentile p $ V.fromList xs
   describe "percentile" $ do
 
     it "finds the p-percentile of the list by sorting" $ do
-      lpercentile 0   uut `shouldBe` 1
-      lpercentile 30  uut `shouldBe` 2
-      lpercentile 50  uut `shouldBe` 3
-      lpercentile 70  uut `shouldBe` 4
-      lpercentile 100 uut `shouldBe` 5
+      percentile 0   uut `shouldBe` 1
+      percentile 30  uut `shouldBe` 2
+      percentile 50  uut `shouldBe` 3
+      percentile 70  uut `shouldBe` 4
+      percentile 100 uut `shouldBe` 5
 
     it "works on all permutations of a list" $ property $
-      forAll (shuffle uut) (\xs -> lpercentile 30 xs == 2)
+      forAll (shuffle uut) (\xs -> percentile 30 xs == 2)
 
     it "gives the maximum for 100th percentile" $ property $
-      forAll (listOf1 doubles) (\xs -> lpercentile 100 xs == maximum xs)
+      forAll (listOf1 doubles) (\xs -> percentile 100 xs == maximum xs)
 
     it "gives the minimum for 0th percentile" $ property $
-      forAll (listOf1 doubles) (\xs -> lpercentile 0 xs == minimum xs)
+      forAll (listOf1 doubles) (\xs -> percentile 0 xs == minimum xs)
 
     it "handles singleton vectors" $ property $
-      forAll (choose (0, 100)) (\p -> lpercentile p [0] == 0)
+      forAll (choose (0, 100))
+             (\p -> percentile p [0::Double] == (0::Double))
 
     -- it "for any p and for any singleton, the percentile is the value"
     -- ^ not sure how to express this just yet. Look at Gen combinators
@@ -86,44 +83,41 @@ spec = do
       shouldBeUndefined $ evaluate (percentile 50 V.empty)
 
 
-  let lmedian = median . V.fromList
   describe "median" $ do
 
     it "picks the middle element of an odd-lengthed list" $ do
-      lmedian [1, 2, 3] `shouldBe` 2
-      lmedian [3, 1, 2] `shouldBe` 2
-      lmedian [31, 111.1, 12, 13, -10] `shouldBe` 13
+      median [1, 2, 3] `shouldBe` 2
+      median [3, 1, 2] `shouldBe` 2
+      median [31, 111.1, 12, 13, -10] `shouldBe` 13
 
     it "accurately interpolates between values in an even-length list" $ do
-      lmedian [1, 2, 3, 4] `shouldBe` 2.5
-      lmedian [3, 4, 2, 1] `shouldBe` 2.5
-      lmedian [141, 16, 1.7, 12] `shouldBe` 14
+      median [1, 2, 3, 4] `shouldBe` 2.5
+      median [3, 4, 2, 1] `shouldBe` 2.5
+      median [141, 16, 1.7, 12] `shouldBe` 14
 
     it "works on all permutations of the list" $ property $
-      forAll (shuffle uut) (\xs -> lmedian xs == 3)
+      forAll (shuffle uut) (\xs -> median xs == 3)
 
     it "gives the single element of a singleton vector" $ property $
-      forAll (vector 1) $ \xs -> lmedian xs == head xs
+      forAll (vector 1) $ \xs -> median xs == (head xs :: Double)
 
     it "is undefined on empty lists" $
       shouldBeUndefined $ evaluate (median V.empty)
 
 
-  let lq1 = q1 . V.fromList
-  let lq3 = q3 . V.fromList
   describe "q1" $ do
 
     it "finds the first quartile of a vector of doubles" $
-      lq1 uut `shouldBe` 1.75
+      q1 uut `shouldBe` 1.75
 
     it "works on all permutations of a list" $ property $
-      forAll (shuffle uut) (\xs -> lq1 xs == 1.75)
+      forAll (shuffle uut) (\xs -> q1 xs == 1.75)
 
     it "is always no greater than q3" $ property $
-      forAll (listOf1 doubles) (\xs -> lq1 xs <= lq3 xs)
+      forAll (listOf1 doubles) (\xs -> q1 xs <= q3 xs)
 
     it "gives the singleton's value when applied to one" $ property $
-      forAll (vector 1) $ \xs -> lq1 xs == head xs
+      forAll (vector 1) $ \xs -> q1 xs == (head xs :: Double)
 
     it "is undefined on empty vectors" $
       shouldBeUndefined $ evaluate (q1 V.empty)
@@ -132,38 +126,25 @@ spec = do
   describe "q3" $ do
 
     it "finds the third quartile of a vector of doubles" $
-      lq3 uut `shouldBe` 4.25
+      q3 uut `shouldBe` 4.25
 
     it "works on all permutations of a list" $ property $
-        forAll (shuffle uut) $ \xs -> lq3 xs == 4.25
+        forAll (shuffle uut) $ \xs -> q3 xs == 4.25
 
     it "is always at least q1" $ property $
-      forAll (listOf1 doubles) (\xs -> lq3 xs >= lq1 xs)
+      forAll (listOf1 doubles) (\xs -> q3 xs >= q1 xs)
 
     it "gives the singleton's value when applied to one" $ property $
-      forAll (vector 1) $ \xs -> lq3 xs == head xs
+      forAll (vector 1) $ \xs -> q3 xs == (head xs :: Double)
 
     it "is undefined on empty vectors" $
       shouldBeUndefined $ evaluate (q3 V.empty)
 
 
-  let liqr = iqr . V.fromList
   describe "iqr" $ do
 
     it "is equivalent to expanding the subtraction expression" $ property $
-      forAll (listOf1 doubles) (\xs -> liqr xs == lq3 xs - lq1 xs)
+      forAll (listOf1 doubles) (\xs -> iqr xs == q3 xs - q1 xs)
 
     it "is undefined on empty vectors" $
       shouldBeUndefined $ evaluate (iqr V.empty)
-
-
-  let lnoOutliers = noOutliers . V.fromList
-  describe "noOutliers" $ do
-
-    it "removes elements greater than 1.5 IQR from the median" $ do
-      lnoOutliers [1, 1, 1, 1, 1, 20] `shouldBe` V.fromList [1, 1, 1, 1, 1]
-      lnoOutliers [100, 93, 60, 96, 91, 99, 100, 95]
-        `shouldBe` V.fromList [100, 93, 96, 91, 99, 100, 95]
-
-    it "returns an empty vector when given one" $
-      lnoOutliers [] `shouldBe` V.fromList []
