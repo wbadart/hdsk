46b2eb44e3645634f3d2d8408633f7bf382d77fd
Will Badart <wbadart@live.com>
2018-08-20 14:04:11 -0700

    Implemented 1-pass kmeans Assigns cluster labels to data points, does not yet iterate to improve centroids. Planning on structuring that with an external improvement algorithm (a la modular-style numerical methods).
    Added `-Wno-type-defaults` to test compile options.

--- src/Hdsk/Cluster/KMeans.hs
+++ src/Hdsk/Cluster/KMeans.hs
@@ -11,7 +11,41 @@ clusters is known and fixed (it is a parameter to the algorithm).
 
 module Hdsk.Cluster.KMeans
 ( kmeans
+, closestTo
+, minkowski
 ) where
 
-kmeans :: Fractional a => [[a]] -> [a] -> [a]
-kmeans centroids dat = undefined
+import Data.Function (on)
+import Data.List (elemIndex, minimumBy)
+import Data.Maybe (fromMaybe)
+
+type DistFunc a = [a] -> [a] -> a
+
+
+-- | /O(nkD)/ where /n/ is the number of data points, /k/ is the number
+-- of clusters, and D is the dimensionality of the data. Run one
+-- iteration of the k-means algorithm. The parameter /k/, number of
+-- clusters, is implied by the length of the list of initial centroids.
+-- Returns a list of cluster labels (encoded as integers) which
+-- correspond 1-1 with the given list of data points.
+kmeans :: (Ord a, Floating a) => DistFunc a -> [[a]] -> [[a]] -> [Int]
+kmeans dist centroids = map $ toIdx . flip (closestTo dist) centroids
+  where toIdx c = fromMaybe (-1) $ elemIndex c centroids
+
+-- | /O(kD)/ where /k/ is the number of points to consider and /D/ is
+-- the dimensionality of the data. Select from a list of points that
+-- which is closest to a given point /x/ according to a given distance
+-- metric. For instance, in one dimension:
+--
+-- >>> closestTo (\x y -> abs $ sum (zipWith (-) x y)) [0] [[1], [2]]
+-- >>> [1.0]
+closestTo :: (Ord a, Floating a) => DistFunc a -> [a] -> [[a]] -> [a]
+closestTo dist x = minimumBy (compare `on` dist x)
+
+-- | /O(D)/ where /D/ is the dimensionality of the vectors. General
+-- distance metric between two vectors/ data points in the same vector
+-- space. The first parameter @p@ corresponds to the /L^p/ space to
+-- compute the norm (e.g. @p = 2@ is Euclidean distance).
+minkowski :: Floating a => a -> [a] -> [a] -> a
+minkowski p x y = sum (zipWith absDistP x y) ** (1 / p)
+  where absDistP xi yi = abs (xi - yi) ** p
--- test/Hdsk/Cluster/KMeansSpec.hs
+++ test/Hdsk/Cluster/KMeansSpec.hs
@@ -5,24 +5,37 @@ Description:  Unit tests for the Hdsk.Cluster.KMeans exports
 
 module Hdsk.Cluster.KMeansSpec (spec) where
 
-import Control.Exception (evaluate)
-import Data.List (nub)
 import Test.Hspec (Spec, describe, it, shouldBe)
-import Test.QuickCheck (Gen, property)
-
-import Hdsk.Util (shouldBeUndefined)
+import Test.QuickCheck (property)
 
 import Hdsk.Cluster.KMeans
 
 spec :: Spec
-spec =
+spec = do
 
-  describe "kmeans" $
+  let euclidean = minkowski (2 :: Double)
+
+  describe "kmeans" $ do
 
     it "correctly groups close points" $
-      kmeans [[0, 0], [10, 10]]
-             [[1, 0], [0, 1], [9, 8]]
+      kmeans euclidean [[0, 0], [10, 10]]
+                       [[1, 0], [0, 1], [9, 8]]
         `shouldBe` [0, 0, 1]
 
-    it "is undefined for 0 clusters" $
-      shouldBeUndefined $ evaluate (kmeans [] [[1, 1]])
+    it "should always give a cluster index" $ property
+      (\dat -> (-1) `notElem` kmeans euclidean [[0, 0], [10, 10]] dat)
+
+
+  describe "closestTo" $
+
+    it "for point x, selects the point from a list closest to x by `dist`" $
+      closestTo euclidean [1, 1] [[0, 0], [10, 10]] `shouldBe` [0, 0]
+
+
+  describe "minkowski" $ do
+
+    it "finds the manhattan distance for p=1" $
+      minkowski 1 [0, 0] [10, 10] `shouldBe` 20
+
+    it "finds the euclidean distance for p=2" $
+      minkowski 2 [0, 0] [10, 10] `shouldBe` sqrt 200
