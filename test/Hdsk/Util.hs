ba9c7b22ac450ddda3676270b72b4254df9a7192
Will Badart <wbadart@live.com>
2018-08-20 17:50:56 -0700

    Re-implemented helper expectations Removed explicit flip and used function infix syntax instead to bind second argument.

--- test/Hdsk/Util.hs
+++ test/Hdsk/Util.hs
@@ -17,13 +17,13 @@ import Test.QuickCheck (Gen, choose)
 
 shouldLieBetween :: (Real r, Ord r, Show r) =>
   r -> r -> r -> Expectation
-shouldLieBetween n m = flip shouldSatisfy ((&&) <$> (>=n) <*> (<=m))
+shouldLieBetween n m = (`shouldSatisfy` ((&&) <$> (>=n) <*> (<=m)))
 
 shouldBeUndefined :: IO a -> Expectation
-shouldBeUndefined = flip shouldThrow $ errorCall  "Prelude.undefined"
+shouldBeUndefined = (`shouldThrow` errorCall  "Prelude.undefined")
 
 shouldBeErrorEmpty :: IO a -> Expectation
-shouldBeErrorEmpty = flip shouldThrow $ errorCall "empty list"
+shouldBeErrorEmpty = (`shouldThrow` errorCall "empty list")
 
 doubles :: Gen Double
 doubles = choose (-999999999, 999999999)
