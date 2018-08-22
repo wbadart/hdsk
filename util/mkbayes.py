88ef5dccf1cd58b19a079f0d91bb154cd979e343
Will Badart <wbadart@live.com>
2018-08-22 16:38:20 -0700

    Consolidate python helper scripts into util/ One script for simulating a classifier, and the other for generating random instances of the play tennis dataset.

---
+++ util/mkbayes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from collections import OrderedDict
+from random import choice
+
+ATTRS = OrderedDict([
+    ('outlook', ['sunny', 'overcast', 'rain']),
+    ('temperature', ['hot', 'mild', 'cool']),
+    ('humidity', ['high', 'normal']),
+    ('windy', ['false', 'true'])])
+
+LABELS = ['y', 'n']
+
+
+def gen_tuples():
+    return [choice(v) for v in ATTRS.values()] + [choice(LABELS)]
+
+
+def main():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('N', type=int, help='# tuples to print')
+    args = parser.parse_args()
+
+    for _ in range(args.N):
+        print(gen_tuples())
+
+
+if __name__ == '__main__':
+    main()
