9fa1924127696307d9a3ba62b6a3d1759f50676f
Will Badart <wbadart@live.com>
2018-08-24 09:46:34 -0700

    Generalized the data tuple type (knn) Removed the assumption that data instances are lists of fractionals. Now a tuple can be anything with a binary function to Double (distance func).

--- src/Hdsk/NearestNeighbors.hs
+++ src/Hdsk/NearestNeighbors.hs
@@ -18,20 +18,18 @@ import Data.Heap (MaxPrioHeap)
 import Data.Maybe (fromMaybe)
 import qualified Data.Heap as H
 
-import Hdsk.Cluster.KMeans (DistFunc)
-
 -- | /O(nD log k)/ where /D/ is the cost of the distance function.
 -- Select the @k@ nearest training instances to the argument tuple,
 -- according to the given distance metric (greater value means more
 -- dissimilar).
-knn :: Fractional a => DistFunc a -> Int -> [[a]] -> [a] -> [[a]]
+knn :: (tup -> tup -> Double) -> Int -> [tup] -> tup -> [tup]
 knn dist k dat x = map snd (H.toAscList $ foldr nearest H.empty dat)
   where nearest x' h
           | H.size h < k = H.insert (d, x') h
           | d < getMax h = H.insert (d, x') (getTail h)
           | otherwise    = h
           where d = dist x x'
-        getTail :: MaxPrioHeap Double [a] -> MaxPrioHeap Double [a]
+        getTail :: MaxPrioHeap Double tup -> MaxPrioHeap Double tup
         getTail = fromMaybe H.empty . H.viewTail
         getMax = maybe (1/0) fst . H.viewHead
 
