0f785faebbb25b82b72df5bb6dda4fca5b2832f5
Will Badart <wbadart@live.com>
2018-08-24 13:20:52 -0700

    Added a property to knn, maybe fix travis Travis runs python scripts to generate test data

--- test/Hdsk/NearestNeighborsSpec.hs
+++ test/Hdsk/NearestNeighborsSpec.hs
@@ -6,8 +6,10 @@ Description:  Unit test for the KNN classification module
 module Hdsk.NearestNeighborsSpec (spec) where
 
 import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.QuickCheck (Gen, elements, forAll, listOf, property, vectorOf)
 
 import Hdsk.Cluster.KMeans (distEuclidean)
+import Hdsk.Util (doubles)
 
 import Hdsk.NearestNeighbors
 
@@ -26,8 +28,8 @@ spec = do
           dist x y = distEuclidean (init x) (init y)
           getdat fname =
             map (read::String->[Double]) . lines <$> readFile fname
-      train <- getdat "util/irisTrain.csv"
-      test  <- getdat "util/irisTest.csv"
+      train <- getdat "irisTrain.csv"
+      test  <- getdat "irisTest.csv"
       map (classify train) test `shouldBe` map last test
 
   describe "knn" $ do
@@ -41,6 +43,16 @@ spec = do
         , [5, 6] , [6, 6] ]
         [0.5, 0.5] `shouldBe` [[1, 1], [0, 1], [0, 0]]
 
+    it "never returns more than k points" $ property $
+      forAll listOfPair
+             (\ ~(dat, k) -> length (knn distEuclidean k dat [0, 0]) <= k)
+
   describe "majorityLabel" $
     it "reports the most frequent label" $
       majorityLabel id [0, 0, 0, 1, 1] `shouldBe` 0
+
+listOfPair :: Gen ([[Double]], Int)
+listOfPair = do
+  k <- elements [1..99]
+  dat <- listOf (vectorOf 2 doubles)
+  return (dat, k)
