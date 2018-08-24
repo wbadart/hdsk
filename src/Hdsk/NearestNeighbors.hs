3dff66d643ff9ae56d3cf5d20f66674071710eed
Will Badart <wbadart@live.com>
2018-08-23 21:21:18 -0700

    Working on heap-based implementation of knn Iterate over the dataset maintaining a k-sized max priority heap. If the current element is closer than the max near neighbor, replace it with the current element. It's late

--- src/Hdsk/NearestNeighbors.hs
+++ src/Hdsk/NearestNeighbors.hs
@@ -14,13 +14,26 @@ module Hdsk.NearestNeighbors
 , majorityLabel
 ) where
 
+import Data.Heap (MaxPrioHeap)
+import Data.Maybe (fromMaybe)
+import qualified Data.Heap as H
+
 import Hdsk.Cluster.KMeans (DistFunc)
 
 -- | /O(nD)/ where /D/ is the cost of the distance function. Select the
 -- @k@ nearest training instances to the argument tuple, according to
 -- the given distance metric (greater value means more dissimilar).
-knn :: Fractional a => DistFunc a -> Int -> [[a]] -> [[a]]
-knn _ _ _ = undefined
+knn :: Fractional a =>
+    DistFunc a -> Int -> [[a]] -> [a] -> MaxPrioHeap Double [a]
+knn dist k dat x = foldr nearest (H.empty::MaxPrioHeap Double a) dat
+  where nearest x' h =
+          if dist x x' < getMax h
+            then H.insert (dist x x', x')
+               (if H.size h < k
+                  then h
+                  else fromMaybe undefined (H.viewTail h))
+            else h
+        getMax = fst . fromMaybe (1/0) . H.viewHead
 
 -- | /O(???)/ Report the most frequent label from a list of data
 -- instances.
