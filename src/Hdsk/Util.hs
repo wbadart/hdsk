f67bd7fd2165c4947bfb176a0dd1a0f295b97ab3
Will Badart <wbadart@live.com>
2018-08-27 12:00:46 -0700

    Added hashtable-based count implementation Having to use `runST` to extract the counts from `countHT` felt less than ideal, so I added `countElemHT` which encapsulates its usage for the (hopefully) common case of a one-off count of a single element.

--- src/Hdsk/Util.hs
+++ src/Hdsk/Util.hs
@@ -10,12 +10,36 @@ the library. Exporting this because they may be helpful to users too.
 
 module Hdsk.Util
 ( count
+, countHT
+, countElemHT
 ) where
 
+import Control.Monad.ST (ST, runST)
+import Data.Hashable (Hashable)
+import Data.HashTable.ST.Cuckoo (HashTable)
 import Data.Map.Strict (Map)
+import Data.Maybe (fromMaybe)
+
+import qualified Data.HashTable.ST.Cuckoo as H
 import qualified Data.Map.Strict as M
 
--- | /O(n log n)/ Given a container, construct a map from the elements
--- of the container to their frequency within the container.
+-- | /O(n log n)/ Given a container, construct a map from the unique
+-- elements of the container to their frequency within the container.
 count :: (Foldable f, Ord k) => f k -> Map k Int
 count = foldr (flip (M.insertWith (+)) 1) M.empty
+
+-- | /O(n)/ Given a container, construct a hash table from the unique
+-- elements of the container to their frequency within the container. I
+-- recommend this function over 'count' when 1) counting performance is
+-- critical, AND 2) you're familiar with the ST monad.
+countHT :: (Foldable f, Eq k, Hashable k)
+        => f k -> ST s (HashTable s k Int)
+countHT = foldr count' H.new
+  where count' x ht = ht >>= (\h' -> H.mutate h' x (plus1 h'))
+        plus1 h (Just c) = (Just (c + 1), h)
+        plus1 h Nothing  = (Just 1, h)
+
+-- | /O(n)/ Convenience function for getting the count of a single
+-- element from a container.
+countElemHT :: (Foldable f, Eq k, Hashable k) => f k -> k -> Int
+countElemHT xs x = fromMaybe 0 $ runST $ (`H.lookup` x) =<< countHT xs
--- test/Hdsk/UtilSpec.hs
+++ test/Hdsk/UtilSpec.hs
@@ -5,14 +5,28 @@ Description:  Unit tests for the utility module
 
 module Hdsk.UtilSpec (spec) where
 
+import Control.Monad.ST (runST)
 import Test.Hspec (Spec, describe, it, shouldBe)
+import qualified Data.HashTable.Class as H
 import qualified Data.Map.Strict as M
 
 import Hdsk.Util
 
 spec :: Spec
-spec =
+spec = do
+
+  let uut = [1, 1, 2, 3] :: [Int]
 
   describe "count" $
     it "finds the frequency of each element" $
-      count [1, 1, 2, 3] `shouldBe` M.fromList [(1, 2), (2, 1), (3, 1)]
+      count uut `shouldBe` M.fromList [(1, 2), (2, 1), (3, 1)]
+
+  describe "countHT" $
+    it "finds the frequency of each element" $ do
+      let ct = countHT uut
+          expected = [(1, 2), (3, 1), (2, 1)]
+      runST (H.toList =<< ct) `shouldBe` expected
+
+  describe "countElemHT" $
+    it "finds the frequency of one element" $
+      countElemHT uut 1 `shouldBe` 2
