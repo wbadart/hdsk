0eb2781bbee05d505bf98d122429d9a574063285
Will Badart <wbadart@live.com>
2018-08-27 18:55:45 -0700

    Added boilerplate for decision tree modules

---
+++ src/Hdsk/DecisionTree.hs
@@ -0,0 +1,14 @@
+{- |
+Module:       Hdsk.DecisionTree
+Description:  Decision tree classification algorithms
+Copyright:    (c) Will Badart, 2018
+License:      BSD-3-Clause
+
+This module facilitates the definition of decision tree classifiers with
+tools for combining and parametrizing entropy measures, pruning tactics,
+and more.
+-}
+
+module Hdsk.DecisionTree
+(
+) where
---
+++ src/Hdsk/DecisionTree/Entropy.hs
@@ -0,0 +1,13 @@
+{- |
+Module:       Hdsk.DecisionTree.Entropy
+Description:  Measures of data entropy
+Copyright:    (c) Will Badart, 2018
+License:      BSD-3-Clause
+
+This module implements various measures of entropy and information gain
+in service of decision tree classification.
+-}
+
+module Hdsk.DecisionTree.Entropy
+(
+) where
