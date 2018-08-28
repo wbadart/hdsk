3ef3003147916c401b3c77519c74ce121ac28cf8
Will Badart <wbadart@live.com>
2018-08-27 19:01:20 -0700

    Added blank unit test files Tests fail on purpose

---
+++ test/Hdsk/DecisionTree/EntropySpec.hs
@@ -0,0 +1,12 @@
+{- |
+Module:       Hdsk.DecisionTree.EntropySpec
+Description:  Unit tests for measures of entropy
+-}
+
+module Hdsk.DecisionTree.EntropySpec (spec) where
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec =
+  describe "WIP" $ it "should fail" $ 1 `shouldBe` 2
---
+++ test/Hdsk/DecisionTreeSpec.hs
@@ -0,0 +1,12 @@
+{- |
+Module:       Hdsk.DecisionTreeSpec
+Description:  Unit tests of decision tree classifiers
+-}
+
+module Hdsk.DecisionTreeSpec (spec) where
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec =
+  describe "WIP" $ it "should fail" $ 1 `shouldBe` 2
