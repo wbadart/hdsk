9ac5ec54bf5661c4d0c59826c4f3fa98defc0657
Will Badart <wbadart@live.com>
2018-08-27 20:36:20 -0700

    Implemented entropy Currently hard-coded in terms of log base 2 (~ # of bits)

--- src/Hdsk/DecisionTree/Entropy.hs
+++ src/Hdsk/DecisionTree/Entropy.hs
@@ -12,9 +12,16 @@ module Hdsk.DecisionTree.Entropy
 ( entropy
 ) where
 
+import qualified Data.Map.Strict as M
+
+import Hdsk.Util (count)
+
 -- | /O(???)/ Calculate the total information entropy of a dataset.
-entropy :: Floating a => [tup] -> a
-entropy dat = undefined
+entropy :: (Functor f, Foldable f, Eq label, Ord label, Floating a)
+        => (tup -> label) -> f tup -> a
+entropy getLabel dat = M.foldr prob 0 (count (fmap getLabel dat))
+  where prob ct acc = let p = fromIntegral ct / len in acc - p * lg p
+        len = fromIntegral (length dat)
 
 
 -- ===== Utilities ===== --
--- test/Hdsk/DecisionTree/EntropySpec.hs
+++ test/Hdsk/DecisionTree/EntropySpec.hs
@@ -7,6 +7,14 @@ module Hdsk.DecisionTree.EntropySpec (spec) where
 
 import Test.Hspec (Spec, describe, it, shouldBe)
 
+import Hdsk.DecisionTree.Entropy
+
 spec :: Spec
 spec =
-  describe "WIP" $ it "should fail" $ 1 `shouldBe` 2
+
+  describe "entropy" $ do
+    it "finds expected entropy in base 2" $
+      entropy id [1, 1, 2, 2] `shouldBe` 1
+
+    it "is 0 for homogeneous lists" $
+      entropy id [1, 1, 1, 1] `shouldBe` 0
