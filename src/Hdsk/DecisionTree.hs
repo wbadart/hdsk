8e7815116110717bedc7e74f665ccfe226e58488
Will Badart <wbadart@live.com>
2018-08-29 12:31:35 -0700

    Added documentation to information methods

--- src/Hdsk/DecisionTree.hs
+++ src/Hdsk/DecisionTree.hs
@@ -18,19 +18,27 @@ module Hdsk.DecisionTree
 import Hdsk.DecisionTree.Entropy (entropy, conditionalEntropy)
 import Hdsk.Util (length', countBy', lg)
 
--- | /O(???)/
+
+-- ===== Information Measures ===== --
+
+-- | /O(nC + n log n)/ Compute the information gain of the given
+-- branching. Branches are encoded as a list of predicates over data
+-- instances. An object belongs to whichever branch for which it passes
+-- the predicate. Assumes that each data object will pass one and only
+-- one of the branching predicates.
 infoGain :: (Eq label, Ord label)
          => (tup -> label) -> [tup] -> [tup -> Bool] -> Double
 infoGain getLabel dat branches =
   entropy getLabel dat - conditionalEntropy getLabel dat branches
 
--- | /O(???)/
+-- | /O(nB)/ where /B/ is the number of branches. Compute the split info
+-- of the branching.
 splitInfo :: [tup] -> [tup -> Bool] -> Double
 splitInfo dat branches =
   let bits c = (c / length' dat) * lg (c / length' dat)
   in  -sum (map (bits . (`countBy'` dat)) branches)
 
--- | /O(???)/
+-- | /O(nB + nC + n log n)/ Compute the gain ratio of a branching.
 gainRatio :: (Eq label, Ord label)
           => (tup -> label) -> [tup] -> [tup -> Bool] -> Double
 gainRatio getLabel dat branches =
