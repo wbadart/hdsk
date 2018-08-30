727543d6a605e0f7e6901a7f436b33104cfdadc8
Will Badart <wbadart@live.com>
2018-08-30 11:00:22 -0700

    Generalized DecisionTree functions beyond just list Propogated the generalization of the Entropy module up into DecisionTree. Should now work on any containers fitting the Foldable and Alternative constraints (classify also requires MonadPlus for mfilter).

--- src/Hdsk/DecisionTree.hs
+++ src/Hdsk/DecisionTree.hs
@@ -17,48 +17,49 @@ module Hdsk.DecisionTree
 , mkCatTests
 ) where
 
+import Control.Applicative (Alternative)
+import Control.Monad (MonadPlus, mfilter)
 import Data.List (find, maximumBy)
 import Data.Function (on)
 import Hdsk.DecisionTree.Entropy (entropy, conditionalEntropy)
-import Hdsk.Util (length', countBy', lg, majorityLabel)
+import Hdsk.Util (length', countBy', lg, majorityLabel, uniq')
 
-import qualified Data.Set as S
-
--- | /O(???)/ Predict the label of an unobserved tuple, according to the
--- provided parameters.
-classify :: (Eq label, Ord label)
-         => ([tup] -> [[tup -> Bool]]) -- ^ Function to generate possible
+-- | Predict the label of an unobserved tuple, according to the provided
+-- parameters.
+classify :: (Foldable f, Alternative f, MonadPlus f, Eq label, Ord label)
+         => (f tup -> [[tup -> Bool]]) -- ^ Function to generate possible
                                        -- branchings from the data
          -> (tup -> label) -- ^ Function which extracts the label from a
                            -- data object
-         -> ((tup -> label) -> [tup] -> [tup -> Bool] -> Double) -- ^ Split
+         -> ((tup -> label) -> f tup -> [tup -> Bool] -> Double) -- ^ Split
                                  -- criteria, like 'infoGain' or 'gainRatio'
-         -> [tup] -- ^ The dataset as a list of data objects
+         -> f tup -- ^ The dataset as a list of data objects
          -> tup   -- ^ The unobserved instance
          -> label -- ^ The predicted label
 classify mkBranchings getLabel criterion dat tup
-  | length (uniq $ map getLabel dat) <= 1 = majorityLabel getLabel dat
+  | length (uniq' $ fmap getLabel dat) <= 1 = majorityLabel getLabel dat
   | otherwise =
     let branching  = bestBranching getLabel criterion dat branchings
         branchings = mkBranchings dat
      in maybe undefined
-        (\p -> classify' (filter p dat) tup)
+        (\p -> classify' (mfilter p dat) tup)
         $ find ($tup) branching
   where classify' = classify mkBranchings getLabel criterion
 
 -- | /O(n log n)/ Compute the branching predicates for the feature
 -- values at the given index. The feature values at index @idx@ should
 -- be categorical.
-mkCatTests :: Ord a => (tup -> a) -> [tup] -> [tup -> Bool]
-mkCatTests getVal = map (\v -> (==v) . getVal) . uniq . map getVal
+mkCatTests :: (Foldable f, Alternative f, Ord a)
+           => (tup -> a) -> f tup -> f (tup -> Bool)
+mkCatTests getVal = fmap (\v -> (==v) . getVal) . uniq' . fmap getVal
 
 -- | /O(BX)/ where /B/ is the number of branchings to test and /X/ is
 -- the cost of the branching criterion. Find the optimal branching
 -- according to the given criterion.
-bestBranching :: (Eq label, Ord label)
+bestBranching :: (Foldable f, Alternative f, Eq label, Ord label)
               => (tup -> label)
-              -> ((tup -> label) -> [tup] -> [tup -> Bool] -> Double)
-              -> [tup]
+              -> ((tup -> label) -> f tup -> [tup -> Bool] -> Double)
+              -> f tup
               -> [[tup -> Bool]]
               -> [tup -> Bool]
 bestBranching getLabel criterion dat =
@@ -72,28 +73,21 @@ bestBranching getLabel criterion dat =
 -- instances. An object belongs to whichever branch for which it passes
 -- the predicate. Assumes that each data object will pass one and only
 -- one of the branching predicates.
-infoGain :: (Eq label, Ord label)
-         => (tup -> label) -> [tup] -> [tup -> Bool] -> Double
+infoGain :: (Foldable f, Alternative f, Eq label, Ord label)
+         => (tup -> label) -> f tup -> [tup -> Bool] -> Double
 infoGain getLabel dat branches =
   entropy getLabel dat - conditionalEntropy getLabel dat branches
 
 -- | /O(nB)/ where /B/ is the number of branches. Compute the split info
 -- of the branching.
-splitInfo :: (Eq label, Ord label)
-          => (tup -> label) -> [tup] -> [tup -> Bool] -> Double
+splitInfo :: (Foldable f, Alternative f, Eq label, Ord label)
+          => (tup -> label) -> f tup -> [tup -> Bool] -> Double
 splitInfo _ dat branches =
   let bits c = (c / length' dat) * lg (c / length' dat)
   in  -sum (map (bits . (`countBy'` dat)) branches)
 
 -- | /O(nB + nC + n log n)/ Compute the gain ratio of a branching.
-gainRatio :: (Eq label, Ord label)
-          => (tup -> label) -> [tup] -> [tup -> Bool] -> Double
+gainRatio :: (Foldable f, Alternative f, Eq label, Ord label)
+          => (tup -> label) -> f tup -> [tup -> Bool] -> Double
 gainRatio getLabel dat branches =
   infoGain getLabel dat branches / splitInfo getLabel dat branches
-
-
--- ===== Utilities ===== --
-
--- | /O(n log n)/
-uniq :: Ord a => [a] -> [a]
-uniq = S.toList . S.fromList
