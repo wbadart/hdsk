5a5148dfb93ee640a07fcabd1999a3a346cd3f72
Will Badart <wbadart@live.com>
2018-09-01 10:09:03 -0700

    Fixed up NaiveBayes documentation

--- src/Hdsk/NaiveBayes.hs
+++ src/Hdsk/NaiveBayes.hs
@@ -24,30 +24,30 @@ import qualified Data.Map.Strict as M
 -- label.
 type CPT = Map String (Map String Int)
 
--- | /O(n * k * (log l + log m)) where n is the number of data points, k
--- is the length of each data point, l is the number of labels, and m is
--- the gretest number of feature values for any feature./ Generate the
--- conditional probability tables over the dataset. Counts the occurences
--- of each feature value given a class. Maps from class label to a map
--- from feature value to count.
+-- | /O(n * k * (log l + log m))/ where /n/ is the number of data
+-- points, /k/ is the length of each data point, /l/ is the number of
+-- labels, and /m/ is the gretest number of feature values for any
+-- feature. Generate the conditional probability tables over the
+-- dataset. Counts the occurences of each feature value given a class.
+-- Maps from class label to a map from feature value to count.
 mkTables :: [[String]] -> [CPT]
 mkTables dataset = foldr updateTables initialTables dataset
   where updateTables tup = (zipWith $ incVal (last tup)) tup
         initialTables    = map (const M.empty) $ init (head dataset)
 
--- | /O(l k m) where l is the number of labels, k is the length of each
--- feature vector, and m is the number of feature values./ Given a list
--- of conditional probability tables and an unlabeled tuple, predicts
--- the label of the tuple.
+-- | /O(l k m)/ where /l/ is the number of labels, /k/ is the length of
+-- each feature vector, and /m/ is the number of feature values. Given a
+-- list of conditional probability tables and an unlabeled tuple,
+-- predicts the label of the tuple.
 classify :: [CPT] -> [String] -> String
 classify [] _ = undefined
 classify cpts@(cpt1:_) tup =
   maximumBy (compare `on` posterior cpts tup) $ M.keys cpt1
 
--- | /O(k m) where k is the length of the feature vector and m is the
--- number of feature values./ Given conditional probability tables, a
+-- | /O(k m)/ where /k/ is the length of the feature vector and /m/ is
+-- the number of feature values. Given conditional probability tables, a
 -- tuple, and a label calculate the posterior probability of the label
--- given that tuple. P(C|X)
+-- given that tuple. /P(C|X)/
 posterior :: [CPT] -> [String] -> String -> Double
 posterior [] _ _ = undefined
 posterior cpts@(cpt1:_) tup c =
@@ -55,22 +55,22 @@ posterior cpts@(cpt1:_) tup c =
   where prior = labelCount cpt1 c / dataLength cpt1  -- P(c)
 
 -- | /O(m)/ Given a class, feature value, and conditional probability
--- table, compute the likelihood of that configuration. P(x_i|c)
+-- table, compute the likelihood of that configuration. /P(x_i|c)/
 likelihood :: String -> String -> CPT -> Double
 likelihood c x cpt = fromIntegral ((cpt M.! c) M.! x) / labelCount cpt c
 
 
 -- ===== Utilities ===== --
 
--- | /O(log l + log m) where l is the number of labels and m is the number
--- of feature values./ Used for zipping together a data tuple and the list
--- of conditional probability tables.
+-- | /O(log l + log m)/ where /l/ is the number of labels and /m/ is the
+-- number of feature values. Used for zipping together a data tuple and
+-- the list of conditional probability tables.
 incVal :: String -> String -> CPT -> CPT
 incVal c val table = M.insert c byLabel table
   where byLabel = M.insertWith (+) val 1
                 $ M.findWithDefault M.empty c table
 
--- | /O(m) where m is the number of feature values./ Computes the
+-- | /O(m)/ where /m/ is the number of feature values. Computes the
 -- frequency of the given label from the given conditional prob. table.
 labelCount :: Num n => CPT -> String -> n
 labelCount cpt c = sum $ map fromIntegral (M.elems (cpt M.! c))
