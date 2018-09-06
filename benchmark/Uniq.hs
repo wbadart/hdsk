0f943776c5709887498f9af8b96f0ef3090b37bb
Will Badart <wbadart@live.com>
2018-09-05 21:40:59 -0400

    Added benchmark for nuniq Wanted to see if it could replace uniq' in some decision tree functions (since we weren't interested in the uniq values themselves, just whether or not they were there).
    It's about 3x faster than uniq' but still an order of magnitude slower
    than uniq. There must be some pretty serious optimizations happinging in
    Data.Set.fromList.

--- benchmark/Uniq.hs
+++ benchmark/Uniq.hs
@@ -1,11 +1,13 @@
 import Criterion.Main (defaultMain, env, nf, bench, bgroup)
 
-import Hdsk.Util (uniq, uniq')
+import Hdsk.Util (uniq, uniq', nuniq)
 
 main = defaultMain
   [ bgroup "all unique values"
     [ bench "uniq"  $ nf uniq  [(1::Int)..10000]
-    , bench "uniq'" $ nf uniq' [(1::Int)..10000] ]
+    , bench "uniq'" $ nf uniq' [(1::Int)..10000]
+    , bench "nuniq" $ nf nuniq [(1::Int)..10000] ]
   , bgroup "one unique values"
     [ bench "uniq"  $ nf uniq  $ replicate 10000 (0::Int)
-    , bench "uniq'" $ nf uniq' $ replicate 10000 (0::Int) ]]
+    , bench "uniq'" $ nf uniq' $ replicate 10000 (0::Int)
+    , bench "nuniq" $ nf nuniq $ replicate 10000 (0::Int) ]]
