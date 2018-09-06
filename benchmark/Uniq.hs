c268b88a970fe1ae21a55f47f6ab9dc22ed60351
Will Badart <wbadart@live.com>
2018-09-05 20:15:06 -0400

    Corrected benchmark descriptions

--- benchmark/Uniq.hs
+++ benchmark/Uniq.hs
@@ -3,9 +3,9 @@ import Criterion.Main (defaultMain, env, nf, bench, bgroup)
 import Hdsk.Util (uniq, uniq')
 
 main = defaultMain
-  [ bgroup "no unique values"
+  [ bgroup "all unique values"
     [ bench "uniq"  $ nf uniq  [(1::Int)..10000]
     , bench "uniq'" $ nf uniq' [(1::Int)..10000] ]
-  , bgroup "all unique values"
+  , bgroup "one unique values"
     [ bench "uniq"  $ nf uniq  $ replicate 10000 (0::Int)
     , bench "uniq'" $ nf uniq' $ replicate 10000 (0::Int) ]]
--- src/Hdsk/DecisionTree.hs
+++ src/Hdsk/DecisionTree.hs
@@ -10,62 +10,40 @@ and more.
 -}
 
 module Hdsk.DecisionTree
-( classify
-, mkCatTests
-, mkContTests
+( DecisionTree(..)
 ) where
 
 import Control.Applicative (Alternative)
-import Control.Monad (MonadPlus, mfilter)
-import Data.List (find, maximumBy)
-import Data.Function (on)
-import Hdsk.Util (length', countBy', lg, majorityLabel, uniq')
+import Hdsk.DecisionTree.Information (infoGain)
+import Hdsk.Util (head', majorityLabel, uniq')
 
--- | Predict the label of an unobserved tuple, according to the provided
--- parameters.
-classify :: (Foldable f, Alternative f, MonadPlus f, Eq label, Ord label)
-         => (f tup -> [[tup -> Bool]]) -- ^ Function to generate possible
-                                       -- branchings from the data
-         -> (tup -> label) -- ^ Function which extracts the label from a
-                           -- data object
-         -> ((tup -> label) -> f tup -> [tup -> Bool] -> Double) -- ^ Split
-                                 -- criteria, like 'infoGain' or 'gainRatio'
-         -> f tup -- ^ The dataset as a list of data objects
-         -> tup   -- ^ The unobserved instance
-         -> label -- ^ The predicted label
-classify mkBranchings getLabel criterion dat tup
-  | length (uniq' $ fmap getLabel dat) <= 1 = majorityLabel getLabel dat
-  | otherwise =
-    let branching  = bestBranching getLabel criterion dat branchings
-        branchings = mkBranchings dat
-     in maybe undefined
-        (\p -> classify' (mfilter p dat) tup)
-        $ find ($tup) branching
-  where classify' = classify mkBranchings getLabel criterion
+-- | Representation of a decision tree's structure. Non-leaf nodes are
+-- encoded with a predicate over the tuple type (this function signals
+-- whether a tuple belongs to the branch) and its child nodes. Leaf
+-- nodes, or decisions, likewise claim tuples with their embedded
+-- predicate, and store the appropriate label.
+data DecisionTree tup label
+  -- | The first field of a branching node is the predicate which
+  -- encodes it (e.g. "this is the branch for tuples with attribute
+  -- weather = 'sunny'), and the second is the node's children.
+  = Branches (tup -> Bool) [DecisionTree tup label]
+  -- | Again, the first field determines which data objects this node
+  -- applies to, and the second records the decision of the node.
+  | Decision (tup -> Bool) label
 
--- | /O(n log n)/ Compute the branching predicates for the feature
--- values at the given index. The feature values at index @idx@ should
--- be categorical.
-mkCatTests :: (Foldable f, Alternative f, Ord a)
-           => (tup -> a) -> f tup -> f (tup -> Bool)
-mkCatTests getVal = fmap (\v -> (==v) . getVal) . uniq' . fmap getVal
-
--- | /O(???)/ Compute the branching predicates for a continuous
--- predicate. Will always be a pair, where one branch claims the objects
--- with values less than or equal to the split point, and the other
--- those greater. The split optimizes the given criterion.
-mkContTests :: (Foldable  f, Alternative f, Ord a)
-            => (tup -> a) -> f tup -> f (tup -> Bool)
-mkContTests _ _ = undefined
-
--- | /O(BX)/ where /B/ is the number of branchings to test and /X/ is
--- the cost of the branching criterion. Find the optimal branching
--- according to the given criterion.
-bestBranching :: (Foldable f, Alternative f, Eq label, Ord label)
-              => (tup -> label)
-              -> ((tup -> label) -> f tup -> [tup -> Bool] -> Double)
-              -> f tup
-              -> [[tup -> Bool]]
-              -> [tup -> Bool]
-bestBranching getLabel criterion dat =
-    maximumBy (compare `on` criterion getLabel dat)
+-- | /O(???)/ Generate a decision tree using the ID3 algorithm.
+id3 :: (Alternative f, Foldable f, Ord label)
+    => label
+    -> (tup -> Bool)
+    -> (tup -> label)
+    -> [tup -> v]
+    -> f tup
+    -> DecisionTree tup label
+id3 fallback prop getLabel unused dat
+  | homogenous  = Decision prop (getLabel $ head' dat)
+  | null unused = Decision prop (majorityLabel getLabel dat)
+  | null dat    = Decision prop fallback
+  | otherwise   = Branches prop bestBranching
+  where homogenous = length (uniq' $ getLabel <$> dat) == 1
+        bestBranching :: [DecisionTree tup label]
+        bestBranching = undefined
