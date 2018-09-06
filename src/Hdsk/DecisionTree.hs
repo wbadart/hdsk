9e5e3ba786e0eb19cc86c5f5fe24f33641f27d65
Will Badart <wbadart@live.com>
2018-09-06 09:15:07 -0400

    Some quick cosmetic changes

--- src/Hdsk/DecisionTree.hs
+++ src/Hdsk/DecisionTree.hs
@@ -37,7 +37,7 @@ data DecisionTree tup label
 -- /D/ is the depth of the tree. Use the given decision tree to predict
 -- a label for an unobserved data instance.
 classify :: DecisionTree tup label -> tup -> label
-classify (Decision p label) tup = label
+classify (Decision _ label) tup = label
 classify (Branches _ kids)  tup = maybe undefined (`classify` tup)
                                 $ find match kids
   where match (Branches p _) = p tup
